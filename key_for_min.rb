2343097752c4340cc98d221707f60fe18c970720
zahrank21 <zahrank@gmail.com>
2018-01-07 10:00:11 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,5 +2,5 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-  name_hash.sort_by{|k,v| v}.first
+  name_hash.sort_by{|k,v| v}.first[0]
 end
